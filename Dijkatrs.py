8e2af0110038c842970448c06ad7f4ab02705e8c
vishrutsinghal <33685814+vishrutsinghal@users.noreply.github.com>
2018-11-21 16:55:05 +0530

    Update Dijkatrs.py

--- Dijkatrs.py
+++ Dijkatrs.py
@@ -36,3 +36,41 @@ for node in graph["a"]:
     print (node)
 # distances = graph["a"][visited]
 # print(distances)
+
+
+
+
+
+
+    if start==end:
+        path=[]
+        while end != None:
+            path.append(end)
+            end=predecessors.get(end,None)
+        return distances[start], path[::-1]
+    # detect if it's the first time through, set current distance to zero
+    if not visited: distances[start]=0
+    # process neighbors as per algorithm, keep track of predecessors
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            neighbordist = distances.get(neighbor,sys.maxsize)
+            tentativedist = distances[start] + graph[start][neighbor]
+            if tentativedist < neighbordist:
+                distances[neighbor] = tentativedist
+                predecessors[neighbor]=start
+    # neighbors processed, now mark the current node as visited
+    visited.append(start)
+    # finds the closest unvisited node to the start
+    unvisiteds = dict((k, distances.get(k,sys.maxsize)) for k in graph if k not in visited)
+    closestnode = min(unvisiteds, key=unvisiteds.get)
+    # now we can take the closest node and recurse, making it current
+    return shortestpath(graph,closestnode,end,visited,distances,predecessors)
+
+if __name__ == "__main__":
+    graph = {'a': {'w': 14, 'x': 7, 'y': 9},
+            'b': {'w': 9, 'z': 6},
+            'w': {'a': 14, 'b': 9, 'y': 2},
+            'x': {'a': 7, 'y': 10, 'z': 15},
+            'y': {'a': 9, 'w': 2, 'x': 10, 'z': 11},
+            'z': {'b': 6, 'x': 15, 'y': 11}}
+    print (shortestpath(graph,'a','b'))
